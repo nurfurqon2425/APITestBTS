eb0c4c99c3fcdd09d24cdfae0da02d4243ccdcbb
nurfurqon2425 <58716228+nurfurqon2425@users.noreply.github.com>
2024-03-14 16:37:09 +0700

    Add function to checklist and checklistiitem controller

--- APITestBTS/Controllers/ChecklistController.cs
+++ APITestBTS/Controllers/ChecklistController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,57 @@ using System.Threading.Tasks;
 
 namespace APITestBTS.Controllers
 {
+    [ApiController]
+    [Route("checklist")]
     public class ChecklistController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _db;
+        private readonly IConfiguration _configuration;
+        private readonly JwtService _jwtService;
+
+        public ChecklistController(AppDbContext db, IConfiguration configuration)
         {
-            return View();
+            _db = db;
+            _configuration = configuration;
+            _jwtService = new JwtService(_configuration["Jwt:Key"]);
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Checklist>>> GetChecklist()
+        {
+            var checklist = await _db.Checklist.ToListAsync();
+            return Ok(checklist);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Checklist>> Create(Checklist checklist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.Checklist.Add(checklist);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Create), new { id = checklist.ChecklistId }, checklist);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string checklistid)
+        {
+            var checklist = await _db.Checklist.FindAsync(checklistid);
+            if (checklist == null)
+            {
+                return NotFound();
+            }
+
+            _db.Checklist.Remove(checklist);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        
     }
 }
--- APITestBTS/Controllers/ChecklistItemController.cs
+++ APITestBTS/Controllers/ChecklistItemController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,47 @@ using System.Threading.Tasks;
 
 namespace APITestBTS.Controllers
 {
+    [ApiController]
+    [Route("checklist")]
     public class ChecklistItemController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _db;
+        private readonly IConfiguration _configuration;
+        private readonly JwtService _jwtService;
+
+        public ChecklistItemController(AppDbContext db, IConfiguration configuration)
         {
-            return View();
+            _db = db;
+            _configuration = configuration;
+            _jwtService = new JwtService(_configuration["Jwt:Key"]);
         }
+
+        [HttpGet("{checklistid}/item")]
+        public async Task<ActionResult<ChecklistItem>> GetCheclistItem(string checklistid)
+        {
+            var checklistitem = await _db.ChecklistItem.FindAsync(checklistid);
+
+            if (checklistitem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(checklistitem);
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<ChecklistItem>> Create(ChecklistItem checklistitem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.ChecklistItem.Add(checklistitem);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Create), new { id = checklistitem.ChecklistItemId }, checklistitem);
+        }
+
     }
 }
